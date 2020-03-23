2191f84ae93bb41eb0bf23354c65299f188a89d6
ruchika <ruchika6698@gmail.com>
2020-03-23 14:00:24 +0530

    Added:Gambler Win or Loose

--- GamblingSimulation.sh
+++ GamblingSimulation.sh
@@ -4,3 +4,20 @@ echo "-------------------Welcome to Gambling Simulator Program-------------"
 
 STAKE=100
 BET=1
+BetAmount=$STAKE
+
+function betting()
+{
+	Number=$((RANDOM%2))
+
+	if [[ $Number -eq 1 ]]
+	then
+		echo "you won 1 bet"
+		((BetAmount++))
+	else
+		echo "you loose 1 bet"
+		((BetAmount--))
+	fi
+	echo $BetAmount
+}
+betting
