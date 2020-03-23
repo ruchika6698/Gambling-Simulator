45b3c8627ca013f3400575b03171d1b453baa120
ruchika <ruchika6698@gmail.com>
2020-03-23 14:59:08 +0530

    Added:Calculate Daily Winning or loosing amount

--- GamblingSimulation.sh
+++ GamblingSimulation.sh
@@ -2,29 +2,56 @@
 
 echo "-------------------Welcome to Gambling Simulator Program-------------"
 
+#constants
 STAKE=100
 BET=1
+DAYS=20
+
+#variable
+totalWin=0
+totalLoss=0
+count=0
 
 BetAmount=$STAKE
 
 #Function for bet win or loose
 function betting()
 {
-	Number=$((RANDOM%2))
-
-	if [[ $Number -eq 1 ]]
-	then
-		echo "you won 1 bet"
-		((BetAmount++))
-	else
-		echo "you loose 1 bet"
-		((BetAmount--))
-	fi
-	echo $BetAmount
+	for (( count=1; count<=$DAYS; count++ ))
+	do
+      BetAmount=100
+      totalWin=0
+      totalLoss=0
+		while [[ $BetAmount -ne 0 ]]
+		do
+			Number=$((RANDOM%2))
+			if [[ $Number -eq 1 ]]
+			then
+	    		((BetAmount++))
+	    		if [[ $BetAmount -eq $maxLimit ]]
+	    		then
+					break
+        		fi
+        		else
+	    			((BetAmount--))
+	   			if [[ $BetAmount -eq $minLimit ]]
+	    			then
+               	break
+            	fi
+        		fi
+		done
+		if [[ $BetAmount -eq $maxLimit ]]
+		then
+			totalWin=$(($maxLimit-$STAKE))
+		else
+			totalLoss=$(($STAKE-$minLimit))
+		fi
+		echo "day: $count  Amount: $BetAmount  Win: $totalWin  Loss:$totalLoss"
+done
 }
 betting
 
-#function for maximum and mininmum limit 
+#function for maximum and mininmum limit
 function Limit()
 {
 	maxLimit=$(($BetAmount+$BetAmount*50/100))
