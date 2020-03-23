ed56a994c631fb9c8660b2e0716762879dab6e56
ruchika <ruchika6698@gmail.com>
2020-03-23 14:18:56 +0530

    Added:Gambler Win or loose 50% of stake should resign for day

--- GamblingSimulation.sh
+++ GamblingSimulation.sh
@@ -4,8 +4,10 @@ echo "-------------------Welcome to Gambling Simulator Program-------------"
 
 STAKE=100
 BET=1
+
 BetAmount=$STAKE
 
+#Function for bet win or loose
 function betting()
 {
 	Number=$((RANDOM%2))
@@ -21,3 +23,11 @@ function betting()
 	echo $BetAmount
 }
 betting
+
+#function for maximum and mininmum limit 
+function Limit()
+{
+	maxLimit=$(($BetAmount+$BetAmount*50/100))
+	minLimit=$(($BetAmount-$BetAmount*50/100))
+}
+Limit
