b568d347abb7f988392ca7f832cd1cdc2fc692d5
ruchika <ruchika6698@gmail.com>
2020-03-23 15:18:10 +0530

    Added:Each month how much day won or loss

--- GamblingSimulation.sh
+++ GamblingSimulation.sh
@@ -2,32 +2,61 @@
 
 echo "-------------------Welcome to Gambling Simulator Program-------------"
 
+#constants
 STAKE=100
 BET=1
 
+#variable
+totalWin=0
+totalLoss=0
+winAmount=0
+looseAmount=0
+
+read -p "Enter the days : " DAYS
 BetAmount=$STAKE
 
 #Function for bet win or loose
 function betting()
 {
-	Number=$((RANDOM%2))
-
-	if [[ $Number -eq 1 ]]
-	then
-		echo "you won 1 bet"
-		((BetAmount++))
-	else
-		echo "you loose 1 bet"
-		((BetAmount--))
-	fi
-	echo $BetAmount
+	for (( count=1; count<=$DAYS; count++ ))
+	do
+		while [[ $BetAmount -ne 0 ]]
+		do
+			Number=$((RANDOM%2))
+			if [[ $Number -eq 1 ]]
+			then
+	    		((BetAmount++))
+	    		if [[ $BetAmount -eq $maxLimit ]]
+	    		then
+					break
+        		fi
+			else
+				((BetAmount--))
+				if [[ $BetAmount -eq $minLimit ]]
+				then
+					break
+				fi
+			fi
+		done
+		if [[ $BetAmount -eq $maxLimit ]]
+		then
+			totalWin=$(($maxLimit-$STAKE))
+			winAmount=$(($winAmount+$totalWin))
+		else
+			totalLoss=$(($STAKE-$minLimit))
+			looseAmount=$(($looseAmount+50))
+		fi
+		echo "day: $count  Amount: $BetAmount  Win: $totalWin  Loss:$totalLoss WinAmount:$winAmount LooseAmount:$looseAmount"
+	done
 }
-betting
 
-#function for maximum and mininmum limit 
+
+#function for maximum and mininmum limit
 function Limit()
 {
 	maxLimit=$(($BetAmount+$BetAmount*50/100))
 	minLimit=$(($BetAmount-$BetAmount*50/100))
 }
+
+betting
 Limit
